fb846c06a27eae7804e1b6054e043ffa971f56c0
hrmny <github.leah@hrmny.sh>
2023-07-05 20:21:12 +0200

    merge external require of the build runtime and dev node runtime

--- crates/turbopack-ecmascript-runtime/src/dev_runtime.rs
+++ crates/turbopack-ecmascript-runtime/src/dev_runtime.rs
@@ -7,39 +7,31 @@ use turbopack_core::{
     context::AssetContext,
     environment::{ChunkLoading, EnvironmentVc},
 };
-use turbopack_ecmascript::StaticEcmascriptCodeVc;
 
-use crate::{asset_context::get_runtime_asset_context, embed_file_path};
+use crate::{asset_context::get_runtime_asset_context, embed_js::embed_static_code};
 
 /// Returns the code for the development ECMAScript runtime.
 #[turbo_tasks::function]
 pub async fn get_dev_runtime_code(environment: EnvironmentVc) -> Result<CodeVc> {
     let asset_context = get_runtime_asset_context(environment);
 
-    let shared_runtime_utils_code =
-        StaticEcmascriptCodeVc::new(asset_context, embed_file_path("shared/runtime-utils.ts"))
-            .code();
+    let shared_runtime_utils_code = embed_static_code(asset_context, "shared/runtime-utils.ts");
+    let runtime_base_code = embed_static_code(asset_context, "dev/runtime/base/runtime-base.ts");
 
-    let runtime_base_code = StaticEcmascriptCodeVc::new(
-        asset_context,
-        embed_file_path("dev/runtime/base/runtime-base.ts"),
-    )
-    .code();
+    let chunk_loading = &*asset_context
+        .compile_time_info()
+        .environment()
+        .chunk_loading()
+        .await?;
 
-    let runtime_backend_code = StaticEcmascriptCodeVc::new(
+    let runtime_backend_code = embed_static_code(
         asset_context,
-        match &*asset_context
-            .compile_time_info()
-            .environment()
-            .chunk_loading()
-            .await?
-        {
-            ChunkLoading::None => embed_file_path("dev/runtime/none/runtime-backend-none.ts"),
-            ChunkLoading::NodeJs => embed_file_path("dev/runtime/nodejs/runtime-backend-nodejs.ts"),
-            ChunkLoading::Dom => embed_file_path("dev/runtime/dom/runtime-backend-dom.ts"),
+        match chunk_loading {
+            ChunkLoading::None => "dev/runtime/none/runtime-backend-none.ts",
+            ChunkLoading::NodeJs => "dev/runtime/nodejs/runtime-backend-nodejs.ts",
+            ChunkLoading::Dom => "dev/runtime/dom/runtime-backend-dom.ts",
         },
-    )
-    .code();
+    );
 
     let mut code: CodeBuilder = CodeBuilder::default();
 
@@ -55,6 +47,11 @@ pub async fn get_dev_runtime_code(environment: EnvironmentVc) -> Result<CodeVc> 
 
     code.push_code(&*shared_runtime_utils_code.await?);
     code.push_code(&*runtime_base_code.await?);
+
+    if matches!(chunk_loading, ChunkLoading::NodeJs) {
+        code.push_code(&*embed_static_code(asset_context, "shared-node/require.ts").await?);
+    }
+
     code.push_code(&*runtime_backend_code.await?);
 
     // Registering chunks depends on the BACKEND variable, which is set by the
